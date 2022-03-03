a4d54d846e558fcb37d7c31d2695689e9a2062f8
leonardo1313 <leonardo.novak5@gmail.com>
2022-03-03 22:56:11 +0100

    Initial commit
    - main activity and player setup activity layout done

---
+++ app/src/main/java/com/optimahorizonapps/tictactoe_game/PlayerSetup.java
@@ -0,0 +1,14 @@
+package com.optimahorizonapps.tictactoe_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_setup);
+    }
+}
